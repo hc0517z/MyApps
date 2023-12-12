96c6f7e2f8cbbc4dfdb0235852b220c8170a2001
_mozartkkk <hc0517z@gmail.com>
2023-12-12 15:01:04 +0900

    비동기 메서드를 위한 MemoryRepository 리팩터링
    비동기 작업의 우선 순위를 지정하기 위해 MemoryRepository.cs의 코드 전체가 변경되었습니다. AddAsync, UpdateAsync, DeleteAsync, Clear, GetByIdAsync 및 GetAllAsync 메서드는 비동기/대기 패턴을 활용하도록 수정되었습니다. 이를 통해 리소스를 더욱 효율적으로 사용할 수 있으며 애플리케이션의 전반적인 응답성이 향상됩니다.

--- Infrastructure/MemoryRepository.cs
+++ Infrastructure/MemoryRepository.cs
@@ -6,45 +6,44 @@ using System.Threading.Tasks;
 namespace MyApps.Infrastructure
 {
     public abstract class MemoryRepository<T> : IRepository<T> where T : Entity
+{
+    protected MemoryStore<T> _store = new();
+    public int Count => _store.Count;
+
+    public virtual async Task<T> AddAsync(T newItem)
     {
-        protected MemoryStore<T> _store = new();
-
-        public int Count => _store.Count;
-
-        public virtual Task<T> AddAsync(T newItem)
-        {
-            _store.Add(newItem.Id, newItem);
-            return Task.FromResult(newItem);
-        }
-
-        public virtual Task<T> UpdateAsync(T newItem)
-        {
-            _store[newItem.Id] = newItem;
-            return Task.FromResult(newItem);
-        }
-
-        public virtual Task<T> DeleteAsync(Guid id)
-        {
-            var byId = GetByIdAsync(id).Result;
-            _store.Remove(byId.Id);
-            return Task.FromResult(byId);
-        }
-
-        public virtual Task Clear()
-        {
-            _store.Clear();
-            return Task.CompletedTask;
-        }
-
-        public Task<T> GetByIdAsync(Guid id)
-        {
-            var entity = _store.TryGetValue(id, out var value) ? value : null;
-            return Task.FromResult(entity);
-        }
-
-        public Task<IEnumerable<T>> GetAllAsync()
-        {
-            return Task.FromResult(_store.Values.ToList().AsEnumerable());
-        }
+        _store.Add(newItem.Id, newItem);
+        return await Task.FromResult(newItem);
     }
+
+    public virtual async Task<T> UpdateAsync(T newItem)
+    {
+        _store[newItem.Id] = newItem;
+        return await Task.FromResult(newItem);
+    }
+
+    public virtual async Task<T> DeleteAsync(Guid id)
+    {
+        var byId = await GetByIdAsync(id);
+        _store.Remove(byId.Id);
+        return await Task.FromResult(byId);
+    }
+
+    public virtual async Task Clear()
+    {
+        _store.Clear();
+        await Task.CompletedTask;
+    }
+
+    public async Task<T> GetByIdAsync(Guid id)
+    {
+        var entity = _store.GetValueOrDefault(id);
+        return await Task.FromResult(entity);
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await Task.FromResult(_store.Values.ToList().AsEnumerable());
+    }
+}
 }
