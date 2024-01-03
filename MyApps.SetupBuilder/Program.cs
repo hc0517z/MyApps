f131ea4e3cc57b1309bea52436ce8168c4cca3e3
_mozartkkk <hc0517z@gmail.com>
2024-01-03 15:23:47 +0900

    프로젝트 구성 변경 및 설치파일 생성 프로젝트 추가

---
+++ MyApps.SetupBuilder/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using WixSharp;
+
+namespace MyApps.SetupBuilder
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var version = "1.0.0.1";
+            var project = new Project("MyApps",
+                new Dir(@"%LocalAppDataFolder%\Programs\MyApps",
+                    new Files(@"..\MyApps\bin\Release\net6.0-windows\*.*", f =>
+                        f.EndsWith(".exe") ||
+                        f.EndsWith(".dll") ||
+                        f.EndsWith(".deps.json") ||
+                        f.EndsWith("runtimeconfig.json")),
+                    new Dir(@"%ProgramMenu%\MyApps",
+                        new ExeFileShortcut("MyApps", Path.Combine("[INSTALLDIR]", "MyApps.exe"), ""),
+                        new ExeFileShortcut("Uninstall MyApps", "[System64Folder]msiexec.exe", "/x [ProductCode]")),
+                    new Dir(@"%Desktop%",
+                        new ExeFileShortcut("MyApps", Path.Combine("[INSTALLDIR]", "MyApps.exe"), ""))
+                ))
+            {
+                ControlPanelInfo =
+                {
+                    ProductIcon = @"..\MyApps\icon.ico"
+                },
+                Version = new Version(version),
+                UI = WUI.WixUI_ProgressOnly,
+                GUID = new Guid("6f330b47-2577-43ad-0517-1861ba25889b"),
+                OutFileName = $"Setup_MyApps_v{version}"
+            };
+            Compiler.BuildMsi(project);
+        }
+    }
+}
